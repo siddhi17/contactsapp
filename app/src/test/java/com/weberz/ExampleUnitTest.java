b6bd7e3cf45b219b904cb757ea83c1ebc1cf6025
siddhi17 <siddhijambhale@gmail.com>
2016-11-07 17:30:15 +0530

    07/11/2016

---
+++ app/src/test/java/com/weberz/ExampleUnitTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * Created by Siddhi on 10/27/2016.
+ */
+
+
+
+
+
+            /**
+     + * To work on unit tests, switch the Test Artifact in the Build Variants view.
+     + */
+            public class ExampleUnitTest {
+           @Test
+           public void addition_isCorrect() throws Exception {
+                   assertEquals(4, 2 + 2);
+               }
+            }
+
